788c08af55c505e63eabe0207feb79404357b139
Osilos <f.marianacci@gmail.com>
2025-03-07 14:16:35 +0100

    chore: add hook on android activity

--- android/src/main/java/com/admobironsourceadapter/AdmobIronsourceAdapterModule.kt
+++ android/src/main/java/com/admobironsourceadapter/AdmobIronsourceAdapterModule.kt
@@ -1,19 +1,36 @@
 package com.admobironsourceadapter
 
+import com.facebook.react.bridge.LifecycleEventListener
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
 import com.ironsource.mediationsdk.IronSource
 
 @ReactModule(name = AdmobIronsourceAdapterModule.NAME)
-class AdmobIronsourceAdapterModule(reactContext: ReactApplicationContext) :
-  NativeAdmobIronsourceAdapterSpec(reactContext) {
+class AdmobIronsourceAdapterModule(private val reactContext: ReactApplicationContext) :
+  NativeAdmobIronsourceAdapterSpec(reactContext), LifecycleEventListener {
+
+  init {
+    reactContext.addLifecycleEventListener(this)
+  }
 
   override fun getName(): String {
     return NAME
   }
 
   override fun setConsent(consent: Boolean) {
-      IronSource.setConsent(consent)
+    IronSource.setConsent(consent)
+  }
+
+  override fun onHostResume() {
+    IronSource.onResume(reactContext.currentActivity)
+  }
+
+  override fun onHostPause() {
+    IronSource.onPause(reactContext.currentActivity)
+  }
+
+  override fun onHostDestroy() {
+      // Cleanup if needed
   }
 
   companion object {
